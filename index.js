c0c6b617716b6a91ccab33d982c843220ce5e407
fb07 <fbrangel27@gmail.com>
2022-11-07 19:58:38 -0600

    Added require onto our HTML labels

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Linked!");
+const name = document.getElementById("inputName")
+const description = document.getElementById("inpuDescription")
+const assignedTo = document.getElementById("inputAssignedTo")
+const dueDate = document.getElementById("inputDueDate")
+
+const validFormFieldInput = (data) => {
+    document.getElementById("inputName") 
+}
+
