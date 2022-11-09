fa5b558fa58731708d8ac0234feb8682cd7c6896
Denise Lewis <denisepl28@gmail.com>
2022-11-09 11:29:22 -0500

    added a form to html file

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const name = document.getElementById('inputName')
-/*const description = document.getElementById('inpuDescription')
-const assignedTo = document.getElementById('inputAssignedTo')
-const dueDate = document.getElementById('inputDueDate')
-const errorElement = document.getElementById('error')*/
+const description = document.getElementById('description')
+const assigned = document.getElementById('assigned')
+const date = document.getElementById('date')
+const errorElement = document.getElementById('error')
 
 /* const validFormFieldInput = (data) => {
     document.getElementById("inputName") 
 } */
-form.addEventListener('button', (e) => {
+form.addEventListener('submit', (e) => {
     let messages = []
     if (inputName.value === '' || inputName.value === null) {
         messages.push('Name is required')
@@ -15,6 +15,6 @@ form.addEventListener('button', (e) => {
 
     if (messages.length > 0) {
         e.preventDefault()
-        errorElement.innerText = messages.join(' , ')
+        errorElement.innerText = messages.join(', ')
     }
 })
