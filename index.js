9ad143aa37cd7b0a9e8fd9391071759b62df07fd
fb07 <fbrangel27@gmail.com>
2022-11-09 09:58:29 -0600

    Updates

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const name = document.getElementById("inputName")
-const description = document.getElementById("inpuDescription")
-const assignedTo = document.getElementById("inputAssignedTo")
-const dueDate = document.getElementById("inputDueDate")
+const name = document.getElementById('inputName')
+/*const description = document.getElementById('inpuDescription')
+const assignedTo = document.getElementById('inputAssignedTo')
+const dueDate = document.getElementById('inputDueDate')
+const errorElement = document.getElementById('error')*/
 
-const validFormFieldInput = (data) => {
+/* const validFormFieldInput = (data) => {
     document.getElementById("inputName") 
-}
+} */
+form.addEventListener('button', (e) => {
+    let messages = []
+    if (inputName.value === '' || inputName.value === null) {
+        messages.push('Name is required')
+    }
 
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(' , ')
+    }
+})
